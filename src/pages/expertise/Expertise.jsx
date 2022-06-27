3ba3a213c65b4c34400cbaf008f60f5b1d47eaf9
Vitalie <vitalie.cociug@tarainteractive.com>
2022-06-27 17:32:29 +0300

    first commit

--- src/pages/expertise/Expertise.jsx
+++ src/pages/expertise/Expertise.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import HeroSection from '../../components/cards/HeroSection'
+import CardOverview from '../../components/cards/CardOverview'
+import Connor from '../../components/images/ConnorWine'
 
 const Expertise = () => {
-  return <div>Expertise</div>;
-};
+	// Card content
+	const cardContent = {
+		imageSrc: Connor,
+		name: 'Connor Colquhoun',
+		position: 'wine connoisseur',
+		country: 'japan',
+	}
 
-export default Expertise;
+	return (
+		<Container>
+			<HeroSection />
+			<Row>
+				<Col className="col-8">
+					<p>
+						Sit excepteur ullamco tempor excepteur fugiat amet culpa consectetur
+						ea ut fugiat. Laborum irure non qui nulla minim anim reprehenderit
+					</p>
+				</Col>
+				<Col>
+					<CardOverview {...cardContent} />
+					<CardOverview {...cardContent} />
+				</Col>
+			</Row>
+		</Container>
+	)
+}
+
+export default Expertise
