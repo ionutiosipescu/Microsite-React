7fa1407fa63bf447f3e2fa86404396b136140042
unknown <anton.ostopovici@tarainteractive.com>
2022-06-27 16:26:13 +0300

    Create routes links , and folder for every-page

--- src/routes/routes/dashboard.js
+++ src/routes/routes/dashboard.js
@@ -1,28 +1,64 @@
-import { lazy } from 'react'
-import About from '../../pages/about/About'
-import Dashboard from '../../pages/dashboard/Dashboard'
+import { lazy } from "react";
+import About from "../../pages/about/About";
+import Contact from "../../pages/contact/Contact";
+import Dashboard from "../../pages/dashboard/Dashboard";
+import Expertise from "../../pages/expertise/Expertise";
+import Home from "../../pages/home/Home";
+import Insight from "../../pages/insight/Insight";
+import Leadership from "../../pages/leadership/Leadership";
 
 export const routeNames = {
-	about: '/',
-	dashboard: '/dashboard',
-}
+  home: "/",
+  expertise: "/expertise",
+  insight: "/insight",
+  leadership: "/leadership",
+  contact: "/contact",
+
+  about: "/about",
+  dashboard: "/dashboard",
+};
 
 const routes = [
-	// {
-	// 	path: routeNames.dashboard,
-	// 	exact: true,
-	// 	component: lazy(() => import('../../pages/Home')),
-	// },
-	{
-		path: routeNames.about,
-		exact: true,
-		element: <About />,
-	},
-	{
-		path: routeNames.dashboard,
-		exact: true,
-		element: <Dashboard />,
-	},
-]
+  // {
+  // 	path: routeNames.dashboard,
+  // 	exact: true,
+  // 	component: lazy(() => import('../../pages/Home')),
+  // },
+  {
+    path: routeNames.home,
+    exact: true,
+    element: <Home />,
+  },
+  {
+    path: routeNames.expertise,
+    exact: true,
+    element: <Expertise />,
+  },
+  {
+    path: routeNames.insight,
+    exact: true,
+    element: <Insight />,
+  },
+  {
+    path: routeNames.leadership,
+    exact: true,
+    element: <Leadership />,
+  },
+  {
+    path: routeNames.contact,
+    exact: true,
+    element: <Contact />,
+  },
+  {
+    path: routeNames.about,
+    exact: true,
+    element: <About />,
+  },
+  {
+    path: routeNames.dashboard,
+    exact: true,
+    element: <Dashboard />,
+  },
+];
 
-export default routes
+export default routes;
