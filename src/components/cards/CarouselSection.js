0db7f63d37cf307a75c5302dc30aee749a25a1c6
unknown <anton.ostopovici@tarainteractive.com>
2022-06-29 16:43:26 +0300

    Insights Pages

--- src/components/cards/CarouselSection.js
+++ src/components/cards/CarouselSection.js
@@ -1,73 +1,79 @@
-import React from 'react'
-import styled from 'styled-components'
-import digital from '../../components/images/digital'
-import { Carousel } from 'react-bootstrap'
+import React from "react";
+import styled from "styled-components";
+import digital from "../../components/images/digital";
+import { Carousel } from "react-bootstrap";
 
 //style to overload the carousel's indicators
-import './CarouselSection.scss'
+import "./CarouselSection.scss";
 
 const CarouselContainer = styled.div`
-	// height: 320px;
-	width: 100%;
-	//   background-color: white;
-	background-color: ${props => props.backgroundColor || ''};
-	color: ${props => props.textColor || 'white'};
-	h3 {
-		color: ${props => props.titleColor || ''};
-		font-size: 18px;
-	}
-	p {
-		font-size: 14px;
-	}
-`
+  // height: 320px;
+  width: 100%;
+  //   background-color: white;
+  background-color: ${(props) => props.backgroundColor || ""};
+  color: ${(props) => props.textColor || "white"};
+
+  .text-date {
+    color: ${(props) => props.textDate || "white"};
+  }
+  h3 {
+    color: ${(props) => props.titleColor || ""};
+    font-size: 18px;
+  }
+  p {
+    font-size: 14px;
+  }
+`;
 
 const CarouselSection = ({
-	categoryCarousel,
-	backgroundColor,
-	titleColor,
-	textColor,
-	arr,
+  categoryCarousel,
+  backgroundColor,
+  titleColor,
+  textColor,
+  arr,
+  textDate,
 }) => {
-	return (
-		<CarouselContainer
-			backgroundColor={backgroundColor}
-			titleColor={titleColor}
-			textColor={textColor}
-			className="container p-4"
-		>
-			<h3>{categoryCarousel || ''}</h3>
-			<Carousel className="carousel1">
-				<Carousel.Item className="row">
-					<div className="d-flex justify-content-around">
-						<div className="col-sm-12 col-md-11 col-lg-5 m-4 ">
-							<h6>{arr[0].title}</h6>
-							<span className="text-muted"> {arr[0].date}</span>
-							<p className="py-4">{arr[0].desciption}</p>
-						</div>
-						<div className="col-sm-12 col-md-11 col-lg-5 m-4">
-							<h6>{arr[1].title}</h6>
-							<span className="text-muted "> {arr[1].date}</span>
-							<p className="py-4">{arr[1].desciption}</p>
-						</div>
-					</div>
-				</Carousel.Item>
-				<Carousel.Item>
-					<div className="d-flex justify-content-around">
-						<div className="col-sm-12 col-md-11 col-lg-5 m-4">
-							<h6>{arr[2].title}</h6>
-							<span className="text-muted  "> {arr[2].date}</span>
-							<p className="py-4">{arr[2].desciption}</p>
-						</div>
-						<div className="col-sm-12 col-md-11 col-lg-5 m-4">
-							<h6>{arr[3].title}</h6>
-							<span className="text-muted "> {arr[3].date}</span>
-							<p className="py-4">{arr[3].desciption}</p>
-						</div>
-					</div>
-				</Carousel.Item>
-			</Carousel>
-		</CarouselContainer>
-	)
-}
+  return (
+    <CarouselContainer
+      backgroundColor={backgroundColor}
+      titleColor={titleColor}
+      textColor={textColor}
+      textDate={textDate}
+      className="container p-4"
+    >
+      <h3>{categoryCarousel || ""}</h3>
+      <Carousel className="carousel1">
+        <Carousel.Item className="row h-100">
+          <div className="d-flex justify-content-around ">
+            <div className="col-sm-12 col-md-11 col-lg-5 m-4 ">
+              <h6>{arr[0].title}</h6>
+              <span className="text-date"> {arr[0].date}</span>
+              <p className="py-4">{arr[0].desciption}</p>
+            </div>
+            <div className="col-sm-12 col-md-11 col-lg-5 m-4">
+              <h6>{arr[1].title}</h6>
+              <span className="text-date "> {arr[1].date}</span>
+              <p className="py-4">{arr[1].desciption}</p>
+            </div>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item className="row h-100">
+          <div className="d-flex justify-content-around ">
+            <div className="col-sm-12 col-md-11 col-lg-5 m-4  ">
+              <h6>{arr[2].title}</h6>
+              <span className="text-date"> {arr[2].date}</span>
+              <p className="py-4">{arr[2].desciption}</p>
+            </div>
+            <div className="col-sm-12 col-md-11 col-lg-5 m-4 ">
+              <h6>{arr[3].title}</h6>
+              <span className="text-date"> {arr[3].date}</span>
+              <p className="py-4">{arr[3].desciption}</p>
+            </div>
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </CarouselContainer>
+  );
+};
 
-export default CarouselSection
+export default CarouselSection;
--- src/components/icons.js
+++ src/components/icons.js
@@ -1,14 +1,26 @@
-const iconsPath = '/icons/'
+const iconsPath = "/icons/";
 
 const icon = (className, name) => (
-	<img className={className} src={iconsPath + name + '.svg'} alt=" " />
-)
+  <img className={className} src={iconsPath + name + ".svg"} alt=" " />
+);
+const iconPng = (className, name) => (
+  <img className={className} src={iconsPath + name + ".png"} alt=" " />
+);
 
-const Search = ({ className }) => icon(className, 'search')
-const ChevronRight = ({ className }) => icon(className, 'chevronRight')
-const LinkedIn = ({ className }) => icon(className, 'linkedin')
-const YouTube = ({ className }) => icon(className, 'youtube')
-const Facebook = ({ className }) => icon(className, 'facebook')
-const Twitter = ({ className }) => icon(className, 'twitter')
+const Search = ({ className }) => icon(className, "search");
+const ChevronRight = ({ className }) => icon(className, "chevronRight");
+const LinkedIn = ({ className }) => icon(className, "linkedin");
+const YouTube = ({ className }) => icon(className, "youtube");
+const Facebook = ({ className }) => icon(className, "facebook");
+const Twitter = ({ className }) => icon(className, "twitter");
+const ChevronRightBlue = ({ className }) => iconPng(className, "chevron");
 
-export { Search, ChevronRight, LinkedIn, Facebook, Twitter, YouTube }
+export {
+  Search,
+  ChevronRight,
+  ChevronRightBlue,
+  LinkedIn,
+  Facebook,
+  Twitter,
+  YouTube,
+};
