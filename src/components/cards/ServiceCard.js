2d15fdb21f4352eb9ce72050ed3c4abdaf0689d4
unknown <anton.ostopovici@tarainteractive.com>
2022-07-14 14:39:18 +0300

    services

---
+++ src/components/cards/ServiceCard.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components";
+import { Row, Col } from "react-bootstrap";
+import YellowButton from "../buttons/YellowButton";
+
+const ServiceCard = ({ data }) => {
+  const ServicesCardContainer = styled.div`
+    .cardTitle {
+      font-size: 18px;
+      font-weight: bold;
+    }
+    .cardDescription {
+      font-size: 14px;
+      font-weight: normal;
+      padding-top: 10px;
+    }
+    button {
+      font-size: 13px;
+      height: 50%;
+      border-radius: 5px;
+    }
+  `;
+  return (
+    <ServicesCardContainer>
+      <Row className="my-4 fw-bold   ">
+        <h3 className=" cardTitle">{data.title}</h3>
+
+        <Row className="border-bottom">
+          <Col lg={8}>
+            <p className="cardDescription">{data.description}</p>
+          </Col>
+
+          <Col lg={4} className="d-flex align-items-center">
+            <YellowButton
+              text={data.textButton ?? "contact our team"}
+              // width="80%"
+              className=""
+            />
+          </Col>
+        </Row>
+      </Row>
+    </ServicesCardContainer>
+  );
+};
+
+export default ServiceCard;
