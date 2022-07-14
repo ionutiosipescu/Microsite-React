ee484f9ead382651547771c930d8c3fedee314c7
ionutiosipescu <ionutiosipescu@yahoo.com>
2022-07-14 16:08:53 +0300

    Brazil in the News

--- src/pages/about/subPages/News.js
+++ src/pages/about/subPages/News.js
@@ -1,7 +1,54 @@
 import React from "react";
+import { Card, Container, Row, Col } from 'react-bootstrap'
+import NewsCard from '../../../components/cards/NewsCard'
 
 const News = () => {
-  return <div>News</div>;
+  let news = {
+		title: 'Card Title',
+		date: '15/5/2020',
+		content:
+			'Some quick example text to build on the card title and make  Some quick example text to build on the card title and make  Some quick example text to build on the card title and make ',
+	}
+  return (
+    <>
+			<Row>
+				<Col>
+					<NewsCard
+						title={news.title}
+						content={news.content}
+						date={news.date}
+					/>
+					<NewsCard
+						title={news.title}
+						content={news.content}
+						date={news.date}
+					/>
+					<NewsCard
+						title={news.title}
+						content={news.content}
+						date={news.date}
+					/>
+				</Col>
+				<Col>
+					<NewsCard
+						title={news.title}
+						content={news.content}
+						date={news.date}
+					/>
+					<NewsCard
+						title={news.title}
+						content={news.content}
+						date={news.date}
+					/>
+					<NewsCard
+						title={news.title}
+						content={news.content}
+						date={news.date}
+					/>
+				</Col>
+			</Row>
+		</>
+  );
 };
 
 export default News;
