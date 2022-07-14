8fd918641820ebe4a6a35df0b034a60351afa6aa
ionutiosipescu <ionutiosipescu@yahoo.com>
2022-07-14 13:05:22 +0300

    Location Cards

--- src/pages/about/subPages/Locations.js
+++ src/pages/about/subPages/Locations.js
@@ -1,7 +1,32 @@
 import React from "react";
+import {Container, Row, Col} from "react-bootstrap";
+import CardsLocations from '../../../components/cards/CardsLocations'
 
 const Locations = () => {
-  return <div>Locations</div>;
+  return <>
+      <Container>
+        <Row>
+          <Col>
+              <h2>
+              Serving businesses, governments and communities worldwide. 
+          </h2>
+          <p>
+          Whether serving as trusted advisers or in interim management roles to accelerate positive change, our professionals are on the ground across North America, Europe, the Middle East, Asia and Latin America.
+          </p>
+          </Col>
+        </Row>
+        <Row className='p-0 m-0 d-flex flex-column flex-lg-row'>
+          <Col lg={6} className="d-flex flex-md-wrap flex-md-row">
+          <CardsLocations/>
+          <CardsLocations/>
+          <CardsLocations/>
+          </Col>
+          <Col lg={6}>
+           
+          </Col>
+        </Row>
+      </Container>
+  </>;
 };
 
 export default Locations;
