79aff0b9a1bee979b90a2462a387e32dfbc4b9d7
unknown <anton.ostopovici@tarainteractive.com>
2022-07-14 14:58:24 +0300

    Simple Map

--- src/components/cards/SimpleMap.js
+++ src/components/cards/SimpleMap.js
@@ -1,32 +1,29 @@
 import React from "react";
-import GoogleMapReact from 'google-map-react';
+import GoogleMapReact from "google-map-react";
 import styled from "styled-components";
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-
-export default function SimpleMap(){
+export default function SimpleMap() {
   const defaultProps = {
     center: {
       lat: 10.99835602,
-      lng: 77.01502627
+      lng: 77.01502627,
     },
-    zoom: 11
+    zoom: 11,
   };
 
   return (
     // Important! Always set the container height explicitly
-    <div style={{ height: '100vh', width: '100%' }}>
+    <div style={{ height: "100vh", width: "100%" }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyB8EZbNyny7DGFI-ZWmVTRqh6Ip_bgCnp4" }}
+        bootstrapURLKeys={{
+          key: "AIzaSyA16d9FJFh__vK04jU1P64vnEpPc3jenec",
+        }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text="My Marker"
-        />
+        <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
       </GoogleMapReact>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/customStyles.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/customStyles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import './styles/main.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
