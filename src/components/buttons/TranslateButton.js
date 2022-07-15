af55269c228ead6eaf1e3a4943d52114d2f06af2
unknown <anton.ostopovici@tarainteractive.com>
2022-07-15 11:38:55 +0300

    Translation Button -> switch buttons

--- src/components/buttons/TranslateButton.js
+++ src/components/buttons/TranslateButton.js
@@ -1,40 +1,54 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const ButtonContainer = styled.div`
-	button {
-		border-radius: 5px;
-		border: none;
-		width: auto;
-		height: auto;
-		padding: 0 1.875rem; 
-		line-height: 2.5rem;
-		color: #000;
-		background-color: #d9d9d9;
-		transition: background-color 0.2s ease-out,color 0.2s ease-out;
-		margin: 40px 0;
-	}
-	.firstlanguage:focus, .secondlanguage:focus {
-		color: #fff;
-   		background-color: #0085ca !important;
-    	cursor: default;
-	}
-	button:hover {
-		color: #fff;
-   		background-color: #cf7f00;
-	}
+  button {
+    border-radius: 5px;
+    border: none;
+    width: auto;
+    height: auto;
+    padding: 0 1.875rem;
+    line-height: 2.5rem;
+    color: #000;
+    background-color: #d9d9d9;
+    transition: background-color 0.2s ease-out, color 0.2s ease-out;
+    margin: 40px 0;
+  }
+  /* .firstlanguage:focus,
+  .secondlanguage:focus {
+    color: #fff;
+    background-color: #0085ca !important;
+    cursor: default;
+  } */
+  button:hover {
+    color: #fff;
+    background-color: #cf7f00;
+  }
+  .active {
+    color: #fff;
+    background-color: #0085ca;
+  }
+`;
 
-`
+const TranslateButton = ({ text1, text2 }) => {
+  const [buttonActive, setButtonActive] = useState(false);
+  return (
+    <ButtonContainer>
+      <button
+        autoFocus
+        className={`firstlanguage ${!buttonActive ? "active" : ""}`}
+        onClick={() => setButtonActive(!buttonActive)}
+      >
+        {text1}
+      </button>
+      <button
+        className={`secondlanguage ${!buttonActive ? "" : "active"}`}
+        onClick={() => setButtonActive(!buttonActive)}
+      >
+        {text2}
+      </button>
+    </ButtonContainer>
+  );
+};
 
-
-const TranslateButton = ({text1, text2}) => {
-
-	return (
-		<ButtonContainer>
-			<button autoFocus className='firstlanguage'>{text1}</button>
-			<button className='secondlanguage'>{text2}</button>
-		</ButtonContainer>
-	)
-}
-
-export default TranslateButton
+export default TranslateButton;
