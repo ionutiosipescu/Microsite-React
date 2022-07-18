f2ec89639d40042c5f72d9b83abda70dd48c7116
Vitalie <vitalie.cociug@tarainteractive.com>
2022-07-18 13:49:57 +0300

    changed the navbars

--- src/pages/brazilian/subPages/Professionals.js
+++ src/pages/brazilian/subPages/Professionals.js
@@ -1,34 +1,38 @@
 import React, { useState } from 'react'
+import { Row } from 'react-bootstrap'
+
+//images
+import image from '../../../assets/images/ConnorWine'
+
+//data
+import { leadersList, links, arr } from '../../../utils/data'
+
+// costum components
+import TranslateButton from '../../../components/buttons/TranslateButton'
+import { LeaadersContainer } from '../../leadership/Leadership'
+import { ProfessionalsCardSmall } from '../../../components/cards'
 import {
 	FiltrationNavbar,
 	NavbarDropdown,
 } from '../../../components/navbarComponents'
-import { FilterBy } from '../../../components'
-import { tags, leadersList, arr, links } from '../../../utils/data'
-import { ProfessionalsCardSmall } from '../../../components/cards'
-import styled from 'styled-components'
-
-export const LeaadersContainer = styled.div`
-	display: grid;
-
-	grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-	gap: 4rem;
-	position: relative;
-`
+import FilterBy from '../../../components/FilterBy'
 
 const Professionals = () => {
+	//state of modal
 	const [openedState, setOpenedState] = useState(
 		Array.from(leadersList, () => false)
 	)
 
 	const [filterByTags, setFilterByTags] = useState([
 		'fuck',
-		'fuck',
 		'fuck2',
 		'fuck3',
 		'fuck4',
+		'fuck',
+		'fuck2',
 	])
 
+	//handle position of modal
 	const handleDisplay = index => {
 		console.log('this is the index', index)
 		if (!openedState[index]) {
@@ -42,17 +46,22 @@ const Professionals = () => {
 
 	return (
 		<>
-			<h2>
-				Qui excepteur aute dolore proident adipisicing ullamco laborum.
-				consectetur.
-			</h2>
-			<p>
-				Et enim ex magna aliqua deserunt aliqua nisi cupidatat. Sit irure aliqua
-				aute aliquip ea nisi culpa aliqua nisi quis nostrud cillum culpa nulla.
-				Cillum do incididunt eu cupidatat. Velit reprehenderit eiusmod anim amet
-				id aliquip enim eiusmod culpa. Consequat quis ut incididunt duis nulla
-				magna eiusmod est nulla eiusmod elit laborum do.
-			</p>
+			<div className="container p-5">
+				<Row>
+					<TranslateButton text1={'English'} text2={'Portuguese'} />
+				</Row>
+				<h2>
+					Qui excepteur aute dolore proident adipisicing ullamco laborum.
+					consectetur.
+				</h2>
+				<p>
+					Et enim ex magna aliqua deserunt aliqua nisi cupidatat. Sit irure
+					aliqua aute aliquip ea nisi culpa aliqua nisi quis nostrud cillum
+					culpa nulla. Cillum do incididunt eu cupidatat. Velit reprehenderit
+					eiusmod anim amet id aliquip enim eiusmod culpa. Consequat quis ut
+					incididunt duis nulla magna eiusmod est nulla eiusmod elit laborum do.
+				</p>
+			</div>
 
 			<FiltrationNavbar
 				searchBar
@@ -65,9 +74,8 @@ const Professionals = () => {
 				<NavbarDropdown data={links}>Something2</NavbarDropdown>
 			</FiltrationNavbar>
 			<FilterBy filterByTags={filterByTags} setFilterByTags={setFilterByTags} />
-			<h3 className="m-3">Leaders</h3>
 
-			{/* <CardProfessionals /> */}
+			<h3 className="m-3">Leaders</h3>
 			<div className="m-4">
 				<LeaadersContainer>
 					{leadersList.map((cardInfo, index) => {
--- src/pages/brazilian/subPages/index.js
+++ src/pages/brazilian/subPages/index.js
@@ -1,5 +1,8 @@
 export { default as Capabilities } from './Capabilities'
+export { default as Services } from './Services'
 export { default as Insights } from './Insights'
+export { default as Locations } from './Locations'
+export { default as News } from './News'
 export { default as NewsRelease } from './NewsRelease'
 export { default as Overview } from './Overview'
 export { default as Practice } from './Practice'
