1d3914753d5a6e21e6f210d6f4ce2a948b9f33f8
unknown <anton.ostopovici@tarainteractive.com>
2022-07-22 14:27:29 +0300

    Home Page , heroSection , Carousels

--- src/API/articles.js
+++ src/API/articles.js
@@ -1,24 +1,52 @@
-import Axios from 'axios'
+import Axios from "axios";
 
-const baseApiUrl = process.env.REACT_APP_BASE_API_URL
+const baseApiUrl = process.env.REACT_APP_BASE_API_URL;
 
-export const getArticle = setContent => {
-	const link =
-		baseApiUrl +
-		'/node/article?include=field_primary_industry&filter[field_primary_industry.name]=healthcare&page[limit]=6&sort=-created'
+export const getArticle = async (setContent) => {
+  const link =
+    baseApiUrl +
+    "/node/article?include=field_primary_industry&filter[field_primary_industry.name]=healthcare&page[limit]=6&sort=-created";
 
-	Axios.get(link).then(res => {
-		const arr = []
+  await Axios.get(link).then((res) => {
+    const arr = [];
 
-		res.data.data.map(item => {
-			let data = {}
-			data.title = item.attributes.title
-			data.teaserText = item.attributes.field_teaser_text
-			data.date = item.attributes.created
-			data.id = item.id
+    res.data.data.map((item) => {
+      let data = {};
+      data.title = item.attributes.title;
+      data.teaserText = item.attributes.field_teaser_text;
+      data.date = item.attributes.created;
+      data.id = item.id;
 
-			arr.push(data)
-		})
-		setContent([...arr])
-	})
-}
+      arr.push(data);
+    });
+    setContent([...arr]);
+  });
+};
+
+export const getCaseStudiesArticles = async (setContent) => {
+  const link =
+    baseApiUrl +
+    "/node/article?include=field_primary_industry&filter[field_primary_industry.name]=healthcare&page[limit]=6&sort=-created";
+  const arr = [];
+  await Axios.get(link)
+    .then((res) => {
+      const articles = res.data.data;
+      console.log(articles);
+      articles.forEach((article) => {
+        // console.log(article.attributes?.field_metatag);
+        let art = {
+          // id: article.id,
+          title: article?.attributes?.title,
+          description: article.attributes?.field_teaser_text,
+          category: article.attributes.field_metatag?.keywords,
+          date: new Date(
+            article.attributes?.revision_timestamp
+          ).toLocaleDateString(),
+        };
+        arr.push(art);
+      });
+    })
+    .catch((err) => console.log(err));
+
+  setContent([...arr]);
+};
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,77 +1,55 @@
-import React from 'react'
-import HeroSection from '../../components/cards/HeroSection'
-import CarouselSection from '../../components/cards/CarouselSection'
-import QualitiesSection from '../../components/section/QualitiesSection'
-import RecongnitionSection from '../../components/section/RecongnitionSection'
+import React, { useState, useEffect } from "react";
+import HeroSection from "../../components/cards/HeroSection";
+import CarouselSection from "../../components/cards/CarouselSection";
+import QualitiesSection from "../../components/section/QualitiesSection";
+import RecongnitionSection from "../../components/section/RecongnitionSection";
 
 //import data
-import { arr2 } from '../../utils/data'
+import { arr2 } from "../../utils/data";
+import {
+  fetchHeroSectionDataHome,
+  getCaseStudiesArticles,
+  getArticle,
+} from "../../API";
 
 const Home = () => {
-	return (
-		<div>
-			<HeroSection
-				title={heroSection.title}
-				backgroundUrl={heroSection.backgroundUrl}
-			/>
-			<CarouselSection
-				categoryCarousel={'Healthcare & Live Sciences News'}
-				backgroundColor="white"
-				arr={arr}
-				titleColor="#0085CA"
-				textColor="#000"
-				textDate="#949494"
-				carouselDotBackground="white"
-			/>
-			<QualitiesSection />
-			<RecongnitionSection arr={arr2} titleSection="Recent Recongnition" />
-			<CarouselSection
-				categoryCarousel={'Healthcare & Live Sciences News'}
-				backgroundColor="#002B49"
-				arr={arr}
-				titleColor="#0085CA"
-				textColor="#fff"
-				textDate="#FFF"
-				carouselDotBackground="#002b49"
-			/>
-		</div>
-	)
-}
+  const [heroSectionData, setHeroSectionData] = useState();
+  const [carouselData, setCarouselData] = useState([]);
 
-export default Home
-const heroSection = {
-	title: 'emea heltcare & live sciences',
-	backgroundUrl:
-		'https://images.pexels.com/photos/2356045/pexels-photo-2356045.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-}
+  useEffect(() => {
+    fetchHeroSectionDataHome(setHeroSectionData);
+    getCaseStudiesArticles(setCarouselData);
+  }, []);
 
-const arr = [
-	{
-		id: 1,
-		title: 'lorem-ipsum.line1',
-		date: new Date().toLocaleDateString(),
-		desciption:
-			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a",
-	},
-	{
-		id: 2,
-		title: 'lorem-ipsum.2',
-		date: new Date().toLocaleDateString(),
-		desciption:
-			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a",
-	},
-	{
-		id: 3,
-		title: 'lorem-ipsum.3',
-		date: new Date().toLocaleDateString(),
-		desciption:
-			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a",
-	},
-	{
-		id: 4,
-		title: 'lorem-ipsum.4',
-		date: new Date().toLocaleDateString(),
-		desciption:
-			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a",
-	},
-]
+  return (
+    <div>
+      <HeroSection
+        title={heroSectionData?.title}
+        backgroundUrl={heroSectionData?.backgroundUrl}
+        buttonText="CONTACT"
+      />
+      <CarouselSection
+        categoryCarousel={carouselData[0]?.category}
+        backgroundColor="white"
+        arr={carouselData}
+        titleColor="#0085CA"
+        textColor="#000"
+        textDate="#949494"
+        carouselDotBackground="white"
+      />
+      <QualitiesSection />
+      <RecongnitionSection arr={arr2} titleSection="Recent Recongnition" />
+      <CarouselSection
+        categoryCarousel={carouselData[0]?.category}
+        backgroundColor="#002B49"
+        arr={carouselData}
+        titleColor="#0085CA"
+        textColor="#fff"
+        textDate="#FFF"
+        carouselDotBackground="#002b49"
+      />
+    </div>
+  );
+};
+
+export default Home;
