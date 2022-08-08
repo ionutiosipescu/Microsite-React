ae7f9c1ef558c992f7c119713949ecd5deaee970
ionutiosipescu <ionutiosipescu@yahoo.com>
2022-08-08 11:26:59 +0300

    de adaugat la test addfilter si removefilter

--- src/store/actions/test.js
+++ src/store/actions/test.js
@@ -1,5 +1,12 @@
+export const ADD_FILTER = "ADD_FILTER";
+export const REMOVE_FILTER = "REMOVE_FILTER";
+
+// cu astea aici merge 🤔
+
+
 export const TEST = "TEST";
 
 export function test(payload) {
   return { type: TEST, payload };
 }
+
