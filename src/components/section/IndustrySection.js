6f487990ca4ec40917efb29e40bac3363a837678
unknown <anton.ostopovici@tarainteractive.com>
2022-08-11 15:39:40 +0300

    industry section

--- src/components/section/IndustrySection.js
+++ src/components/section/IndustrySection.js
@@ -4,40 +4,29 @@ import ButtonWithAnimation from "../buttons/ButtonWithAnimation";
 
 const IndustrySectionContainer = styled.div`
   padding: 20px;
-  /* height: 650px; */
   width: 100%;
   background-color: white;
   display: flex;
   justify-content: center;
   font-family: "Helvetica Neue LT Std", sans-serif;
-  /* font-weight: 300; */
-  .contentContainer {
+  .grayContainer {
     width: 90%;
     height: 90%;
     background-color: #f2f2f2;
-    /* margin-bottom: 20px; */
     padding-bottom: 20px;
-    /* padding: 20px; */
     h2 {
       padding-top: 20px;
       padding-left: 55px;
-
-      /* padding: 20px 55px; */
     }
   }
 
   .infoContainer {
     display: flex;
     flex-wrap: wrap;
-    /* border: 2px solid red; */
-
     justify-content: space-around;
   }
   .industry {
     padding-top: 40px;
-
-    /* border: 2px solid red; */
-
     h2 {
       color: var(--darkBlueHome);
       font-size: 1.5em;
@@ -48,22 +37,13 @@ const IndustrySectionContainer = styled.div`
       color: var(--darkBlueHome);
       font-size: 1rem;
     }
-    /* width: 46%; */
-
-    /* .body {
-      min-height: 100px;
-    } */
-  }
-
-  .buttonContainer {
-    /* justify-content: center; */
   }
 `;
 
 const IndustrySection = ({ industries }) => {
   return (
     <IndustrySectionContainer className="">
-      <div className="contentContainer">
+      <div className="grayContainer">
         <h2 className="">Industries: </h2>
         <div className="infoContainer ">
           {industries.map((industry) => (
