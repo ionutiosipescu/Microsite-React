2b80ed928153a374c19c07db55044663a618ce51
ionutiosipescu <ionutiosipescu@yahoo.com>
2022-08-12 11:19:13 +0300

    clean code footer

--- src/components/layout/FooterNew.js
+++ src/components/layout/FooterNew.js
@@ -6,95 +6,89 @@ import { size } from "../../utils/breakpoints";
 
 
 const FooterContainer = styled.div`
-background-color: var(--darkBlue) !important;
- max-width: 80rem;
- height: auto;
+    background-color: var(--darkBlue) !important;
+    max-width: 80rem;
+    height: auto;
     width: 100%;
-  .logo {
-    img {
-      width: 2.813rem;
-      height: 2.5rem;
+.logo {
+        img {
+        width: 2.813rem;
+        height: 2.5rem;
+        }
     }
-  }
-
 `;
-
 const ContentContainer = styled.div`
-height: 100%;
-width: 100%;
-padding: 3.125rem 4.375rem;
-font-size: 0.75rem;
+    height: 100%;
+    width: 100%;
+    padding: 3.125rem 4.375rem;
+    font-size: 0.75rem;
 `
 const SectionsContainer = styled.div`
-position: relative;
-height: 100%;
+    position: relative;
+    height: 100%;
     padding-bottom: 3.125rem;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    /* align-items: center; */
-    .bulletin-container {
+.bulletin-container {
         justify-content: center;
     }
    
 p {
-    color: #fff;
-}
+        color: #fff;
+    }
 .list-info-container {
-    line-height: 1.6;
+            line-height: 1.6;
     p {
-        margin: 0;
-    }
+            margin: 0;
+        }
     .global-careers-box, .contact-privacy-box {
-        width: 50%;
-    }
+            width: 50%;
+        }
     .global, .contact {
-        margin-bottom: 1.25rem;
+            margin-bottom: 1.25rem;
+        }
+    .privacy {
+            opacity: 0.8;
+        }
+    }
+.logo-container, .list-info-container {
+        margin-bottom: 1.875rem;
+    }
+/* MEDIA */
+    @media ${size.md} {
+        margin-bottom: 0;
+        align-items: unset;
+        flex-direction: row;
+        justify-content: space-between;
+    .logo-container, .list-info-container, .bulletin-container, .contact, .global {
+        margin-bottom: 0 !important;
+    }
+
+    .global-careers-box, .contact-privacy-box {
+        display: flex;
     }
     .privacy {
-        opacity: 0.8;
-    }
-}
-.logo-container, .list-info-container {
-    margin-bottom: 1.875rem;
-}
-
-@media ${size.md} {
-    margin-bottom: 0;
-    align-items: unset;
-    flex-direction: row;
-  justify-content: space-between;
-/* aici */
-  .logo-container, .list-info-container, .bulletin-container, .contact, .global {
-    margin-bottom: 0 !important;
-
-  }
-
-  .global-careers-box, .contact-privacy-box {
-    display: flex;
-  }
-  .privacy {
         padding-left: 6.25rem;
     }
-}
+    }
 `
 const ColContainer = styled.div`
-/* height: 100%; */
-
-display: flex;
+    display: flex;
 .logo {
     svg {
         height: 3.438rem;
         width: auto;
+        }
     }
-}
-@media ${size.md} {
-    .bulletin-box {
-    display: flex;
-    flex-direction: column;
-}
-  }
+    /* MEDIA */
+    @media ${size.md} {
+        .bulletin-box {
+            display: flex;
+            flex-direction: column;
+    }
+    }
 `
 const ColBox = styled.div`
 .bulletin-signup {
@@ -124,16 +118,17 @@ input {
         color: #fff;
     }
     }
-    .icons-container {
+.icons-container {
         img {
             height: 1.563rem;
             width: auto;
         }
-        display: flex;
-        justify-content: end;
-        gap: 1rem;
-        margin-bottom: 3rem;
+            display: flex;
+            justify-content: end;
+            gap: 1rem;
+            margin-bottom: 3rem;
     }
+    /* MEDIA */
     @media ${size.md} {
         .bulletin-signup {
             flex-direction: row;
@@ -145,24 +140,22 @@ input {
     }
 `
 const BottomContainer = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-border-top: 1px solid #fff;
-padding-top: 1.25rem;
-flex-direction: column;
-@media ${size.md} {
-    flex-direction: row;
-    }
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-top: 1px solid #fff;
+    padding-top: 1.25rem;
+    flex-direction: column;
+    /* MEDIA */
+    @media ${size.md} {
+        flex-direction: row;
+        }
 `
 const CopyrightSection = styled.div`
-
-color: #fff;
+    color: #fff;
 `
 const BrandSection = styled.div`
-
-color: #fff;
-
+    color: #fff;
 `
 
 const FooterNew = () => {
