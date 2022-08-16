9dd5b0243678fe5a1a05e9fb53a4b24799c64a78
unknown <anton.ostopovici@tarainteractive.com>
2022-08-16 15:21:37 +0300

    Recognition Section

--- src/components/section/RecongnitionSection.js
+++ src/components/section/RecongnitionSection.js
@@ -1,90 +1,71 @@
 import React from "react";
 import styled from "styled-components";
+import digital from "../../assets/images/digital";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-
+import { arr } from "../../utils/data";
 //style to overload the carousel's indicators
 // import "../cards/CarouselSection.scss";
 
 const CarouselContainer = styled.div`
   // height: 100%;
+  padding-top: 20px;
   width: 100%;
-  background-color: white;
-  //   background-color: ${(props) => props.backgroundColor || ""};
-  padding: 30px 0px;
-  h3 {
-    color: #000;
-    font-weight: 600;
-    font-size: 18px;
+  display: flex;
+  justify-content: center;
+  .contentContainer {
+    width: 88%;
+    h3 {
+      font-family: "Helvetica Neue LT Std", sans-serif;
+      font-weight: 600;
+    }
   }
-  p {
-    font-size: 14px;
-  }
-  .react-multi-carousel-dot button {
-    height: 20px;
-    width: 20px;
-    border-radius: 50%;
-    // color: orange;
-    border: 3px solid #cf7f00 !important;
-    // background: #002b49;
-    background: ${(props) => props.carouselDotBackground || "#fff"};
-  }
-  .react-multi-carousel-dot--active button {
-    background: #cf7f00;
+  .articlesContainer {
+    display: flex;
+    flex-wrap: wrap;
+    /* flex-direction: column; */
   }
 `;
 
 const RecongnitionSection = ({ arr, titleSection }) => {
   return (
-    <CarouselContainer
-      //   backgroundColor={backgroundColor}
-      //   titleColor={titleColor}
-      className="p-4"
-    >
-      <div className="mx-3">
-        <h3>{titleSection || ""}</h3>
-        <Carousel
-          responsive={responsive}
-          removeArrowOnDeviceType={["tablet", "mobile", "desktop"]}
-          // deviceType={this.props.deviceType}
-          // dotListClass="react-multi-carousel-dot-list"
-          showDots
-        >
-          {arr?.map((element, index) => (
-            <div className="py-3 me-5" key={index}>
-              <p
+    <CarouselContainer>
+      <div className="contentContainer">
+        <h3>{titleSection || "Recent Recognition:"}</h3>
+        <div className="articlesContainer">
+          {[1, 2]?.map((element, index) => (
+            <div
+              className="py-3 me-5 col-11 col-sm-11 col-md-5 col-lg-5"
+              key={index}
+            >
+              {/* <p
                 className="fst-italic"
                 dangerouslySetInnerHTML={{ __html: element?.text }}
-              />
+              /> */}
 
-              <span>{element?.title.toUpperCase()}</span>
-              <p className="">{element?.job_title}</p>
+              <p>
+                “Digital has the power to transform operations, propel 
+                commercial improvements and to be the catalyst for care model
+                innovations. David’s in-depth industry sector and digital
+                background dovetails with A&M’s operator bench strength and
+                leadership, action, results credo. He will add value to
+                healthcare and life sciences clients that want to make digital
+                work across their organizations and seek transformative results
+                from laser-focused digital initiatives.”
+              </p>
+
+              <p className="">{element?.job_title ?? "RAYMOND BERGLUND"}</p>
+              <p className="">
+                {element?.job_title ??
+                  `Managing Director with
+                A&M’s Healthcare & Life Sciences`}
+              </p>
             </div>
           ))}
-        </Carousel>
+        </div>
       </div>
     </CarouselContainer>
   );
-};
-
-const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 2,
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 800 },
-    items: 2,
-  },
-  tablet: {
-    breakpoint: { max: 800, min: 545 },
-    items: 1,
-  },
-  mobile: {
-    breakpoint: { max: 545, min: 10 },
-    items: 1,
-  },
 };
 
 export default RecongnitionSection;
