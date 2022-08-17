f54118939241ad4e3908fc718407121cf6620f88
Vitalie <vitalie.cociug@tarainteractive.com>
2022-08-17 17:31:08 +0300

    trying to make page mobile friendly

--- src/pages/insightsPage/styles/inisghts.style.js
+++ src/pages/insightsPage/styles/inisghts.style.js
@@ -1,24 +1,34 @@
 import styled from "styled-components"
 import { size } from "../../../utils/breakpoints"
 
+// export const InsightsContainer = styled.div`
+//   & > :first-child {
+//     /* border: 1px solid black; */
+//     width: 33%;
+//   }
+
+//   & > :nth-child(2) {
+//     width: 33%;
+
+//     /* border: 1px solid green; */
+//   }
+
+//   & > :last-child {
+//     /* border: 1px solid red; */
+//     width: 33%;
+//   }
+
+//   @media ${size.md} {
+//     display: flex;
+//   }
+// `
 export const InsightsContainer = styled.div`
-  & > :first-child {
-    /* border: 1px solid black; */
-    width: 33%;
-  }
+  display: grid;
+  /* grid-template-columns: 1fr 1fr 1fr;
+  display: initial; */
 
-  & > :nth-child(2) {
-    width: 33%;
-
-    /* border: 1px solid green; */
-  }
-
-  & > :last-child {
-    /* border: 1px solid red; */
-    width: 33%;
-  }
-
-  @media ${size.md} {
-    display: flex;
+  @media ${size.lg} {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(33%, 1fr));
   }
 `
