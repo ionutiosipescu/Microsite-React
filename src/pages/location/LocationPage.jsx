7751e71402daa6f6e4f07a2aec281e7a94387e4c
Vitalie <vitalie.cociug@tarainteractive.com>
2022-08-23 16:10:47 +0300

    stuff for ionut

--- src/pages/location/LocationPage.jsx
+++ src/pages/location/LocationPage.jsx
@@ -1,20 +1,60 @@
-import React from "react";
-import { HeroSection } from "../../components/cards";
-import { StyledContainerLocation } from "../../components/layout/Rows&Collumns/Rows&Collumns.style";
-import LocationCardRow from "../../components/cards/LocationCardRow";
-import { objecttest } from "../../utils/data";
+import React from "react"
+import { HeroSection } from "../../components/cards"
+import { StyledContainerLocation } from "../../components/layout/Rows&Collumns/Rows&Collumns.style"
+import LocationCardRow from "../../components/cards/LocationCardRow"
+import { objecttest } from "../../utils/data"
 
 const LocationPage = () => {
-    return(
-        <>
-        <HeroSection height={300} title={"HEALTCARE & LIFE SCIENCES LOCATIONS"} description={"Cross border, no problem. Our global team of senior professionals are ready to execute your business solutions."} />
-        <StyledContainerLocation>
-            <LocationCardRow data={objecttest}/>
-        </StyledContainerLocation>
-        </>
-    )
+  //   let i = [
+  //     {
+  //       country: { name: "Germany", id: 1234 },
+  //       cities: [
+  //         {
+  //           cityName: "Berlin",
+  //           adresa: "somethign so,mthing german",
+  //           telephone: "something",
+  //           // ...
+  //         },
+  //         {
+  //           cityName: "Frankfurt",
+  //           adresa: "somethign soemthing german",
+  //           telephone: "something",
+  //           // ...
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       country: { name: "Uk", id: 1234 },
+  //       cities: [
+  //         {
+  //           cityName: "London",
+  //           adresa: "somethign soemthing german",
+  //           telephone: "something",
+  //           // ...
+  //         },
+  //         {
+  //           cityName: "Bath",
+  //           adresa: "somethign soemthing german",
+  //           telephone: "something",
+  //           // ...
+  //         },
+  //       ],
+  //     },
+  //   ]
+  return (
+    <>
+      <HeroSection
+        height={300}
+        title={"HEALTCARE & LIFE SCIENCES LOCATIONS"}
+        description={
+          "Cross border, no problem. Our global team of senior professionals are ready to execute your business solutions."
+        }
+      />
+      <StyledContainerLocation>
+        <LocationCardRow data={objecttest} />
+      </StyledContainerLocation>
+    </>
+  )
 }
 
-export default LocationPage;
-
-
+export default LocationPage
